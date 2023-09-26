bff965d2cbe3425b4f80cc2f8ba36f78198fd5c8
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2023-09-26 22:06:23 +1000

    Added currentDay function to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
+// Function to display the current date in the header of the page
+
+function currentDay(){
+  var timeNow = dayjs().format('DD MMMM, YYYY')
+  $('#currentDay').text(timeNow)
+}
+currentDay();
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(function () {})
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -18,7 +26,4 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
   
